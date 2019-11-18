2a2d4ef6520a40c4d2f7c92cad89c5a22999f1f6
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-17 17:59:00 -0800

    Set up server

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ const app = express();
 app.get("/", (req, res) => res.send("sup doggy"));
 
 const port = process.env.PORT || 5000;
+
+app.listen( port, () => console.log(`Server is humming on Port  ${port}`))
+
