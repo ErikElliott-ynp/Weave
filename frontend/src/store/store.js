074e702d82e1868d48503eb29d708cf61ba42485
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-18 12:11:11 -0800

    finish session redux cycle

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger"
+import RootReducer from "../reducers/root_reducer"
 
 
 const middlewares = [thunk];
@@ -13,7 +14,7 @@ if (process.env.NODE_ENV !== "production") {
 
 const configureStore = (preloadedState = {}) => {
     return createStore(
-        rootReducer, 
+        RootReducer, 
         preloadedState,
         applyMiddleware(thunk, logger)
     )
