1a6f61d000d417e236be6e3dc24472fb1c66a3dd
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-17 18:32:55 -0800

    Set up db and server

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB as smooth as butter"))
+    .catch( err => console.log(err))
 
 app.get("/", (req, res) => res.send("sup doggy"));
+
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000;
 
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://weaveteam:qbCZTV1A1rCUSzoy@cluster0-clcor.mongodb.net/test?retryWrites=true&w=majority'
+}
+
