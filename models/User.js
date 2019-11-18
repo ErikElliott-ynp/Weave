91b9987fc4b926ec7b1b609a0224e924887b46ed
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-17 21:54:25 -0800

    Add signup route

--- models/User.js
+++ models/User.js
@@ -5,5 +5,15 @@ const UserSchema = new Schema({
     email: {
         type: String,
         required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
     }
 })
+
+module.exports = User = mongoose.model('users', UserSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User')
 
 router.get('./test',  (req, res) => res.json({ msg:  "You made it bud"}));
+
+router.post('/signup', (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then( user => {
+            if (user) {
+                return res.status(400).json({ email: "That Email is already registered"})
+            } else {
+                const newUser = new User({
+                    email: req.body.email,
+                    password: req.body.password
+                })
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then( user => res.json(user))
+                            .catch( err => console.log(err))
+                    })
+                })
+            }
+        })
+})
 
 module.exports = router;
