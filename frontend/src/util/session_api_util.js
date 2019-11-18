52ded9b2a1b2f8395506912f5750498b7d7f0f57
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-18 11:56:04 -0800

    add signup and login api util

--- frontend/src/util/session_api_util.js
+++ frontend/src/util/session_api_util.js
@@ -8,3 +8,11 @@ export const setAuthToken = token  => {
         delete axios.defaults.headers.common['Authoricaitons'];
     }
 };
+
+export const signup = userData => {
+    return axios.post('/api/users/signup', userData);
+}
+
+export const login = userData => {
+    return axios.post('/api/users/login', userData);
+}
