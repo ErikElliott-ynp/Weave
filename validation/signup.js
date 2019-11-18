b4fc9515f2af0ecd344856b5b6efaa960cb99341
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-17 22:15:56 -0800

    Add signup validator

---
+++ validation/signup.js
@@ -0,0 +1,36 @@
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+module.exports = function validateRegisterInput(data) {
+    let errors = {};
+
+    data.email = validText(data.email) ? data.email : "";
+    data.password = validText(data.password) ? data.password : "";
+    data.password2 = validText(data.password2) ? data.password2 : "";
+
+    if (!Validator.isEmail(data.email)) {
+        errors.email = "Email is invalid"
+    }
+
+    if (Validator.isEmpty(data.email)) {
+        errors.email = "An Email is required"
+    }
+
+
+    if (Validator.isEmpty(data.password)) {
+        errors.password = "A Password is required"
+    }
+
+    if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+        errors.password = "Password must be between 6 and 30 characters"
+    }
+
+    if (!Validator.equals(data.password, data.password2)) {
+        errors.password2 = "Passwords must match"
+    }
+
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0
+    }
+}
