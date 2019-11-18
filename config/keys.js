58f13f959209cbf0399a40fea04f822e1348ece3
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-18 10:14:23 -0800

    Fix ignore

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://weaveteam:qbCZTV1A1rCUSzoy@cluster0-clcor.mongodb.net/test?retryWrites=true&w=majority',
-    secretOrKey: 'no:}o>6JR7Q0mSVSNwD=k0#kQYm!iW'
-}
-
