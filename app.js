5ed3008c36aa91765549edc2eb09dd4282fe0944
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-17 18:37:59 -0800

    Add user model page

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 const users = require('./routes/api/users');
+const bodyParser = require('body-parser');
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB as smooth as butter"))
     .catch( err => console.log(err))
 
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => res.send("sup doggy"));
 
 app.use('/api/users', users);
+
 
 const port = process.env.PORT || 5000;
 
