96c97b3ef69b11121ea32fc4b52a1664945a67a5
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-18 10:57:40 -0800

    Add store and listener

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -6,8 +6,12 @@ export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
 export const RECEIVE_SESSIONS_ERRORS = "RECEIVE_SESSION_USER";
 
-export const logout = () => {
+export const logoutUser = () => {
     return {
         type: LOGOUT_CURRENT_USER
     }
+}
+
+export const logout = () => dispatch => {
+    return ""
 }
