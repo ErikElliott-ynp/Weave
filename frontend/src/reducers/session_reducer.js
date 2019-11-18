eb9d797251b9d925ad0cb3f38376f832726160fb
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-18 11:59:41 -0800

    add getstate and dispatch to window

--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -1,7 +1,18 @@
-import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from "../actions/session_actions"
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER, RECEIVE_USER_SIGN_IN } from "../actions/session_actions"
+
+const initialState ={
+    isAuthenicated: false,
+    user: {}
+}
 
 const SessionReducer = ( state = initialState, action ) => {
     switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            return {
+                ...state,
+                isAuthenicated: !!action.currentUser,
+                user: action.RECEIVE_CURRENT_USER
+            }
         case LOGOUT_CURRENT_USER:
             return {
                 isAuthenicated: false,
