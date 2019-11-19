5b57e44949b63c2e8f2aebf521e3dd3442e03c51
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-19 14:30:31 -0800

    add youtube reducer

---
+++ frontend/src/reducers/youtube_videos_reducer.js
@@ -0,0 +1,17 @@
+import { RECEIVE_VIDEOS, RECEIVE_VIDEO } from "../actions/youtube_actions";
+
+const youtubeVideosReducer = (state = {}, action) => {
+    Object.freeze(state);
+    let nextState = Object.assign([], state)
+    switch (action.type) {
+        case RECEIVE_VIDEOS:
+            nextState.concat(action.videos.items)
+            return nextState;
+        case RECEIVE_VIDEO:
+            nextState.push(actions.video.item[0])
+        default:
+            return state;
+    }
+}
+
+export default youtubeVideosReducer
