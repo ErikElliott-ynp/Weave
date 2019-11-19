e2289b33cad77f1d8165d01c9467e201c761a122
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-18 18:20:05 -0800

    fix logout

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -5,7 +5,6 @@ import * as APIUtil from "../util/session_api_util";
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
 export const RECEIVE_SESSIONS_ERRORS = "RECEIVE_SESSION_ERRORS";
-export const RECEIVE_USER_SIGN_IN = "RECEIVE_USER_SIGN_IN";
 
 export const receiveCurrentUser = currentUser => {
     return {
@@ -50,6 +49,6 @@ export const login = user => dispatch => {
 
 export const logout = () => dispatch => {
     localStorage.removeItem('jwtToken')
-    return APIUtil.setAuthToken(false)
-        .then( () => dispatch(logoutUser()));
+    APIUtil.setAuthToken(false)
+    dispatch(logoutUser())
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import configureStore from './/store/store';
 import jwt_decode from 'jwt-decode';
 import { setAuthToken } from "./util/session_api_util";
 import { logout, signup, login } from "./actions/session_actions";
-
+import "../src/styles/app.scss"
 
 document.addEventListener("DOMContentLoaded", () => {
     let store;
@@ -32,6 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     window.signup = signup;
     window.login = login;
+    window.logout = logout;
 
     const root = document.getElementById('root');
 
