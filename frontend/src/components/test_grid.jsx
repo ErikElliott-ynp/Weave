2d5cbe0b7d2db14c178b94d774faba685838f06b
Zak Johnson <fleecemane.lyons@icloud.com>
2019-11-20 10:29:27 -0800

    final styling on login and splash

--- frontend/src/components/test_grid.jsx
+++ frontend/src/components/test_grid.jsx
@@ -7,32 +7,41 @@ class MyFirstGrid extends React.Component {
     render() {
         // layout is an array of objects, see the demo for more complete usage
         var layout = [
-            { i: 'a', x: 0, y: 0, w: 1, h: 2 },
-            { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-            { i: 'c', x: 4, y: 0, w: 1, h: 2 }
+            { i: 'a', x: 3, y: 0, w: 6, h: 2 },
+            { i: 'b', x: 0, y: 0, w: 3, h: 8, minW: 2, maxW: 6 },
+            { i: 'c', x: 4, y: 0, w: 5, h: 5 },
+            { i: 'd', x: 3, y: 0, w: 1, h: 9 },
+            { i: 'e', x: 10, y: 0, w: 3, h: 9 },
         ];
         return (
-            <GridLayout className="layout" layout={layout} cols={12} width={950} rowHeight={30} >
-
-            <div  key="a">
-                <Paper className="finstaTheSpinsta">
-
-                </Paper>
+          <GridLayout
+            className="layout"
+            layout={layout}
+            cols={12}
+            width={950}
+            rowHeight={30}
+          >
+            <div key="a">
+              <Paper className=""></Paper>
             </div>
 
-            <div  key="b">
-                    <Paper className="yeetThatTweet">
-
-                </Paper>
+            <div key="b">
+              <Paper className=""></Paper>
             </div>
 
-            <div  key="c">
-                    <Paper className="rodeIt">
-
-                </Paper>
+            <div key="c">
+              <Paper className=""></Paper>
             </div>
-            </GridLayout>
-        )
+
+            <div key="d">
+              <Paper className=""></Paper>
+            </div>
+
+            <div key="e">
+              <Paper className=""></Paper>
+            </div>
+          </GridLayout>
+        );
     }
 }
 
