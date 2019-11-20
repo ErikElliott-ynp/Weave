02ccf9e2b4a7b4229d3ec05180d6bd2ca0d5e930
Erik Elliott <51393952+ErikElliott-ynp@users.noreply.github.com>
2019-11-20 10:43:39 -0800

    Delete youtube_api_util.js

--- frontend/src/util/youtube_api_util.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios'
-
-export const youtubeMostPopular = () => {
-    return axios.get('https://www.googleapis.com/youtube/v3/search?key=AIzaSyDLtt-3WUmoHD5HVlLloA2IXVIylv6JgP4&part=snippet&chart=mostPopular&regionCode=US')
-}
