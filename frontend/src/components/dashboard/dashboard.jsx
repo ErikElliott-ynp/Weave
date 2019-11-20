c4930b06b7e5a80901788f53b1db38232a87a45d
Al <nguyenvtalexander@gmail.com>
2019-11-20 13:57:46 -0800

    reformatted dash

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-// import YoutubeContainer from "../youtube/youtube_container";
-import Grid from '../test_grid';
+import MyfirstGrid from '../test_grid';
 
 class Dashboard extends React.Component {
     render() {
         return (
-        // <YoutubeContainer />
-            <Grid />
+            <div className='dashboard'>
+                <div className='sidebar'>
+
+                </div>
+                <div className='main'>
+                    <input className='searchbar' type="text" />
+                    <div className='playground'>
+                        <MyfirstGrid />
+                    </div>
+                </div>
+            </div>
         )
     }
 }
