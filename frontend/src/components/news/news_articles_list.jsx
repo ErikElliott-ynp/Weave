a9caee3fae3b71a2243d983ab330fadc1a4f03b8
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-22 16:12:15 -0800

    fixes

--- frontend/src/components/news/news_articles_list.jsx
+++ frontend/src/components/news/news_articles_list.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import NewsArticle from "./news_article"
+import NewsArticle from "./news_article";
+import Button from "@material-ui/core/Button";
 
 function NewsArticlesList(props) {
     const [search, setSearch] = useState("");
@@ -7,6 +8,7 @@ function NewsArticlesList(props) {
     const handleSubmit = (e) => {
         e.preventDefault();
         props.fetchSearchNews(search)
+        setSearch("");
     }
 
     let articlesList = props.articles.map( (article, i) => {
@@ -21,7 +23,16 @@ function NewsArticlesList(props) {
             <div className="search-cont">
                 <form onSubmit={(e) => handleSubmit(e)}>
                     <input type="text" className="search-bar" value={search} onChange={(e) => setSearch(e.currentTarget.value)} placeholder="What topic interests you?"/>
-                    <input type="submit" className="search-btn" value="Search"/>
+                    <Button
+                        id="search-btn"
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className="search-btn"
+                    >
+                        {/* {this.props.formName} */}
+                    </Button>
                 </form>
             </div>
             <ul className="news-list">
