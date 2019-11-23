49045dd2830052bf03aec44f1e33980c3ad957f6
Zak Johnson <fleecemane.lyons@icloud.com>
2019-11-22 16:09:59 -0800

    working api calls to spotify needs further testing in new format

---
+++ frontend/src/util/spotify_util.js
@@ -0,0 +1,52 @@
+import axios from 'axios'
+const clientId = '3f9aba6c3eff4420bc5b9c48eea5f163'
+const clientSecret = '79cceaba56044c2ca709e895ab882441'
+
+export const getAuthToken = () => {
+    axios({
+        url: 'https://accounts.spotify.com/api/token',
+        method: 'post',
+        params: {
+            grant_type: 'client_credentials'
+        },
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        auth: {
+            username: '3f9aba6c3eff4420bc5b9c48eea5f163',
+            password: '79cceaba56044c2ca709e895ab882441'
+        }
+    }).then(function (response) {
+        console.log(response);
+    }).catch(function (error) {
+    });
+
+}
+
+// PLZ DONT NOT COMMIT WITH THE KEY 
+export const fetchTrack = queryString => {
+    axios({
+        url: 'https://api.spotify.com/v1/tracks',
+        method: 'get',
+        params: {
+            q: `${juicy}`,
+            type: 'track'
+        },
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        auth: {
+            username: '3f9aba6c3eff4420bc5b9c48eea5f163',
+            password: '79cceaba56044c2ca709e895ab882441'
+        }
+    }).then(function (response) {
+        console.log(response);
+    }).catch(function (error) {
+    });
+
+}
+
+fetchTrack();
+
