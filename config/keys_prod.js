b9808d8139b16aa95e758520798e5efc4fd94b2b
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-24 20:23:31 -0800

    more keys

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,6 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
     secretOrKey: process.env.SECRET_OR_KEY,
-    youtubeApiKey: process.env.YOUTUBE_API_KEY
+    youtubeApiKey: process.env.YOUTUBE_API_KEY,
+    newsApiKey: process.env.NEWS_API_KEY
 }
