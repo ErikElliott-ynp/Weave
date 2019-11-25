3ec1391f7dbc8a38679a1edf243bc4ce25afebe3
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-24 19:21:26 -0800

    fix heroku bug

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
     secretOrKey: process.env.SECRET_OR_KEY,
-    youtubeApiKey: proccess.env.YOUTUBE_API_KEY
+    youtubeApiKey: process.env.YOUTUBE_API_KEY
 }
