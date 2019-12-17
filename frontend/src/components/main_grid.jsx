74976d68d2f6f09321cbd68d09b7cee070c1f7d4
erikelliott-ynp <erikelliott92@gmail.com>
2019-12-17 11:39:46 -0800

    fix div starting points

--- frontend/src/components/main_grid.jsx
+++ frontend/src/components/main_grid.jsx
@@ -10,9 +10,9 @@ class MainGrid extends React.Component {
     render() {
 
         var layout = [
-          { i: "a", x: 2, y: 0, w: 4.5, h: 10, minW: 4, maxW: 5.5, maxH: 22, minH: 3.5 },
-          { i: "b", x: 2, y: 0, w: 4, h: 8, minW: 2.5, maxW: 8, minH: 6, maxH: 50},
-          { i: "c", x: 2, y: 0, w: 5.5, h: 10, minW: 4.5, MaxW: 6.5, minH: 6, maxH: 50 },
+          { i: "a", x: 0, y: -1.5, w: 4.5, h: 10, minW: 4, maxW: 5.5, maxH: 22, minH: 3.5 },
+          { i: "b", x: 4.7, y: -2, w: 4, h: 8, minW: 2.5, maxW: 8, minH: 6, maxH: 50},
+          { i: "c", x: 2, y: -.5, w: 5.5, h: 10, minW: 4.5, MaxW: 6.5, minH: 6, maxH: 50 },
           { i: "e", x: 2, y: 0, w: 5, h: 8, minW: 1.5, MaxW: 8 },
         ];
         return (
