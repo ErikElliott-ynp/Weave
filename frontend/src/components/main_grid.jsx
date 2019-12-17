9927d4c466a31e8119d1edbc4ae38a58d862fa86
Al <nguyenvtalexander@gmail.com>
2019-12-17 11:46:05 -0800

    fixed resizing

--- frontend/src/components/main_grid.jsx
+++ frontend/src/components/main_grid.jsx
@@ -10,47 +10,37 @@ class MainGrid extends React.Component {
     render() {
 
         var layout = [
-          { i: "a", x: 3, y: 0, w: 6, h: 2 },
-          { i: "b", x: 0, y: 0, w: 3, h: 8, minW: 2, maxW: 6 },
-          { i: "c", x: 4, y: 0, w: 5, h: 5 },
-          { i: "d", x: 3, y: 0, w: 1, h: 9 },
-          { i: "e", x: 10, y: 0, w: 3, h: 9 },
+          { i: "a", x: 0, y: 0, w: 3, h: 5 },
+          { i: "b", x: 10, y: 0, w: 3, h: 5 },
+          { i: "c", x: 0, y: 10, w: 3, h: 5 },
+          { i: "d", x: 10, y: 10, w: 3, h: 5 },
+          { i: "e", x: 10, y: 20, w: 3, h: 5 },
         ];
         return (
           <GridLayout
             className=""
             layout={layout}
-            cols={12}
-            width={1750}
-            rowHeight={30}
+            cols={500}
+            width={1000}
+            rowHeight={10}
             draggableCancel="input"
           >
             <div className='YouTube-box' key="a">
-              <Paper className="">
                 <YoutubeList />
-              </Paper>
             </div>
 
             <div className='GoogleNews-box' key="b">
-              <Paper className="">
                 <NewsList />
-              </Paper>
             </div>
 
             <div className='Imgur-box' key="c">
-              <Paper className="">
                 <ImgurList />
-              </Paper>
             </div>
 
             <div className='Placeholder-box' key="d">
-              <Paper className="">
-                
-              </Paper>
             </div>
 
             <div className='Placeholder-box' key="e">
-              <Paper className=""></Paper>
             </div>
 
           </GridLayout>
