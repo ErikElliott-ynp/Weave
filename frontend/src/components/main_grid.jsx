51719dd2f7eefe99b56a7bcb58a01756aecfcaa8
Al <nguyenvtalexander@gmail.com>
2019-12-18 15:52:48 -0800

    positioning adjustment

--- frontend/src/components/main_grid.jsx
+++ frontend/src/components/main_grid.jsx
@@ -11,8 +11,8 @@ class MainGrid extends React.Component{
     render() {
         var layout = [
           { i: "a", x: 0, y: 0, w: 4.5, h: 15, minW: 4, maxW: 8, maxH: 22, minH: 15 },
-          { i: "b", x: 4.2, y: 0, w: 4, h: 13, minW: 2.5, maxW: 8, minH: 7, maxH: 50},
-          { i: "c", x: 2, y: -.5, w: 5.5, h: 18, minW: 4.5, MaxW: 4.5, minH: 8, maxH: 50 },
+          { i: "b", x: 1, y: -1, w: 4, h: 13, minW: 2.5, maxW: 8, minH: 7, maxH: 50},
+          { i: "c", x: 2, y: 0, w: 5.5, h: 18, minW: 4.5, MaxW: 4.5, minH: 8, maxH: 50 },
         ];
         return (
           <GridLayout
