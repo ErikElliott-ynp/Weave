7b4f7c9f1952a1e1fc44d6d732314a25fd4f4ed4
erikelliott-ynp <erikelliott92@gmail.com>
2019-12-17 18:12:44 -0800

    fix boxes

--- frontend/src/components/main_grid.jsx
+++ frontend/src/components/main_grid.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import GridLayout from 'react-grid-layout';
-import Paper from '@material-ui/core/Paper';
 import YoutubeList from './youtube/youtube_container';
 import NewsList from './news/news_articles_list_container';
 import ImgurList from './imgur/imgur_container';
@@ -9,17 +8,17 @@ class MainGrid extends React.Component {
     render() {
 
         var layout = [
-          { i: "a", x: 0, y: -1.5, w: 4.5, h: 10, minW: 4, maxW: 5.5, maxH: 22, minH: 3.5 },
+          { i: "a", x: 0, y:-1.5, w: 4.5, h: 10, minW: 4, maxW: 8, maxH: 22, minH: 3.5 },
           { i: "b", x: 4.7, y: -2, w: 4, h: 8, minW: 2.5, maxW: 8, minH: 6, maxH: 50},
-          { i: "c", x: 2, y: -.5, w: 5.5, h: 10, minW: 4.5, MaxW: 6.5, minH: 6, maxH: 50 },
+          { i: "c", x: 2, y: -.5, w: 5.5, h: 10, minW: 4.5, MaxW: 4.5, minH: 6, maxH: 50 },
         ];
         return (
           <GridLayout
             className=""
             layout={layout}
-            cols={500}
-            width={1000}
-            rowHeight={10}
+            cols={12}  
+            width={1750}
+            rowHeight={30}
             draggableCancel="input"
           >
             <div className='YouTube-box' key="a">
